c290213d2a066949f50257fcb2ba46cb42032c86
Pratham Purohit <prathamjp02@gmail.com>
2020-04-03 18:54:03 +1100

    added index and static routing

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return("This is the home page...")
+    return render_template("index.html")
 
 @app.route('/hello/<name>')
 def name(name):
@@ -12,3 +12,5 @@ def name(name):
 def about():
     return render_template("about.html")
 
+if __name__ == "main":
+    app.run(DEBUG=True)
